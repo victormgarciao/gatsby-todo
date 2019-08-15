5e0ea8740c7e29c8e093788f8fc1ebec041f3426
Victor Garcia <vimago81@gmail.com>
2019-08-15 17:59:57 +0200

    Structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `TODO APP`,
+    description: `First application with Gatsby and GraphQL`,
+    author: `@Victor`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
+import 'semantic-ui-css/semantic.min.css'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import TodoList from "../components/molecules/TodoList/todoList";
+import AddTodoInput from "../components/molecules/AddTodoInput/addTodoInput";
+import { Segment } from "semantic-ui-react";
+
+const todos = [
+  {
+    description: 'Apples',
+    isChecked: false,
+  },
+  {
+    description: 'Pears',
+    isChecked: true,
+  },
+  {
+    description: 'Bananas',
+    isChecked: false,
+  },
+];
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>ToDos</h1>
+    <p>Add and remove some ToDos.</p>
+    <Segment>
+      <AddTodoInput />
+      <TodoList todos={todos} />
+    </Segment>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )

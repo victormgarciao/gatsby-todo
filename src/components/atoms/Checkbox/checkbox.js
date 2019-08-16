a937b4c092c1ad843a07b9a644a50f1b13c46c91
Victor Garcia <vimago81@gmail.com>
2019-08-16 13:47:54 +0200

    minor change in checkbox component

--- src/components/atoms/Checkbox/checkbox.js
+++ src/components/atoms/Checkbox/checkbox.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from "prop-types"
 
-import { Checkbox as SemCheckbox } from 'semantic-ui-react'
+import { Checkbox as SemUICheckbox } from 'semantic-ui-react'
 
 const Checkbox = (props) => (
-    <SemCheckbox label={props.label} defaultChecked={props.isChecked} />
+    <SemUICheckbox label={props.label} defaultChecked={props.isChecked} />
 )
 
 Checkbox.propTypes = {
